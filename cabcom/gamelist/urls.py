bd59d0e5f1b07529927356de8e44862167551dfb
Steve Maddison <steve@cosam.org>
2014-04-15 22:14:26 +0200

    Gamelist detail view.

--- cabcom/gamelist/urls.py
+++ cabcom/gamelist/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from cabcom.gamelist import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<game_id>\d+)/$', views.detail, name='detail'),
 )
--- cabcom/gamelist/views.py
+++ cabcom/gamelist/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from cabcom.gamelist.models import Data, Game
 
 def index(request):
 	context = {
-		'data_count': Data.objects.count(),
-		'game_count': Game.objects.count(),
+		'first': 1,
+		'last':  Game.objects.count(),
+		'games': Game.objects.all(),
 	}
 	return render(request, 'gamelist/index.html', context)
+
+def detail(request, game_id):
+	game = get_object_or_404(Game, pk=game_id)
+	return render(request, 'gamelist/detail.html', {'game': game})
+
