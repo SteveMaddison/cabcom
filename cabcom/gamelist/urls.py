e92615bd579efd264fb207d129a89fc8e303ec9e
Steve Maddison <steve@cosam.org>
2014-04-16 22:27:56 +0200

    Reuse game list views/templates for game database.

--- cabcom/gamelist/urls.py
+++ cabcom/gamelist/urls.py
@@ -3,5 +3,6 @@ from cabcom.gamelist import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<game_id>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<object_type>\w+)/$', views.index, name='index'),
+	url(r'^(?P<object_type>\w+)/(?P<game_id>\d+)/$', views.detail, name='detail'),
 )
--- cabcom/gamelist/views.py
+++ cabcom/gamelist/views.py
@@ -1,15 +1,37 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from cabcom.gamelist.models import Data, Game
 
-def index(request):
+def index(request, object_type):
+	if object_type == 'game':
+		title = 'Game List'
+		items = Game.objects.all()[0:49]
+		count = Game.objects.count()
+	elif object_type == 'data':
+		title = 'Game Database'
+		items = Data.objects.all()[0:49]
+		count = Data.objects.count()
+	else:
+		raise Http404
+
 	context = {
-		'first': 1,
-		'last':  Game.objects.count(),
-		'games': Game.objects.all(),
+		'title' : title,
+		'first' : 1,
+		'last'  :  50,
+		'count' : count,
+		'games' : items,
+		'object_type': object_type,
 	}
+
 	return render(request, 'gamelist/index.html', context)
 
-def detail(request, game_id):
-	game = get_object_or_404(Game, pk=game_id)
+def detail(request, object_type, game_id):
+	if object_type == 'game':
+		game = get_object_or_404(Game, pk=game_id)
+	elif object_type == 'data':
+		game = get_object_or_404(Data, pk=game_id)
+	else:
+		raise Http404
+
 	return render(request, 'gamelist/detail.html', {'game': game})
 
