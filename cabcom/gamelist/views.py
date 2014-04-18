ffe6441c90d7dab10da1e52c99acab05fc2dfc3b
Steve Maddison <steve@cosam.org>
2014-04-18 21:35:35 +0200

    Search function for games/database.

--- cabcom/gamelist/views.py
+++ cabcom/gamelist/views.py
@@ -12,27 +12,48 @@ class IndexView(RedirectView):
 		return reverse('game')
 
 class GameListView(ListView):
-	queryset = Game.objects.order_by('display_name')
 	template_name = 'gamelist/index.html'
 	context_object_name = 'games'
 	ordering = ['-display_name']
 	paginate_by = 50
+
+	def get_queryset(self):
+		if 'q' in self.request.GET:
+			q = self.request.GET['q']
+			if q:
+				return Game.objects.filter(display_name__icontains = q).order_by('display_name')
+
+		return Game.objects.order_by('display_name')
 
 	def get_context_data(self, **kwargs):
 		context = super(GameListView, self).get_context_data(**kwargs)
 		context['title'] = 'Game List'
 		context['detail_type'] = 'game'
 		context['menu_active'] = 'game'
+
+		if 'q' in self.request.GET:
+			context['search'] = self.request.GET['q']
+
 		return context
 
 class DataListView(GameListView):
-	queryset = Data.objects.order_by('display_name')
+	def get_queryset(self):
+		if 'q' in self.request.GET:
+			q = self.request.GET['q']
+			if q:
+				return Data.objects.filter(display_name__icontains = q).order_by('display_name')
+
+		return Data.objects.order_by('display_name')
 
 	def get_context_data(self, **kwargs):
 		context = super(DataListView, self).get_context_data(**kwargs)
 		context['title'] = 'Game Database'
 		context['detail_type'] = 'data'
 		context['menu_active'] = 'data'
+
+		if 'q' in self.request.GET:
+			context['search'] = self.request.GET['q']
+
 		return context
 
 class GameDetailView(DetailView):
